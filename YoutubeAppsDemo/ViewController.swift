af993a0bdece8d1384009ec908de232a66d7aa95
iosmitDev <mitesh.imply@gmail.com>
2026-07-27 06:03:29 +0530

    initial xcode project creation

---
+++ YoutubeAppsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeAppsDemo
+//
+//  Created by MiteshKumar Patel on 27/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
